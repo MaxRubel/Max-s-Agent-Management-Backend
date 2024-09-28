a0522f6e7a63eb6f09d48b2e08e78c5f7dee0f90
Max Rubel <maxrubel1@gmail.com>
2024-09-27 19:30:33 -0500

    db querying and returning JSON

--- routes/agents.js
+++ routes/agents.js
@@ -7,7 +7,6 @@ router.get("/", async (req, res) => {
   try {
     const agents = await Agent.findAll();
     res.status(200).json(agents);
-    Agent
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
